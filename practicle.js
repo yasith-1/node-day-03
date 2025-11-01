a7957fd3abe8234d13137c1059710822a431de99
Yashith_Prabhashwara <141606159+yasith-1@users.noreply.github.com>
2025-11-01 17:40:55 +0530

    ASYNC -AWAIT

--- practicle.js
+++ practicle.js
@@ -1,3 +1,5 @@
+const { log } = require('console');
+
 const fs = require('fs').promises;
 
 // Callback--------------------------------------------------------------------
@@ -31,15 +33,37 @@ const fs = require('fs').promises;
 
 // Use promises to avoid the callback hell----------------------------------------
 
-fs.readFile('data-1.json', 'utf-8').then((data1) => {
-    console.log("Read file one");
-    return fs.readFile('data-2.json', 'utf-8').then((data2) => ({ data1, data2 }));
-}).then(({ data1, data2 }) => {
-    console.log("Read file two");
-    return fs.readFile('data-3.json', 'utf-8').then((data3) => ({ data1, data2, data3 }));
-}).then(({ data1, data2, data3 }) => {
-    console.log("All Files Read!");
+// fs.readFile('data-1.json', 'utf-8').then((data1) => {
+//     console.log("Read file one");
+//     return fs.readFile('data-2.json', 'utf-8').then((data2) => ({ data1, data2 }));
+// }).then(({ data1, data2 }) => {
+//     console.log("Read file two");
+//     return fs.readFile('data-3.json', 'utf-8').then((data3) => ({ data1, data2, data3 }));
+// }).then(({ data1, data2, data3 }) => {
+//     console.log("All Files Read!");
+//     console.log(data1, data2, data3);
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+
+// Promises also a little bit complicated so as a solution we use ASYNC -AWAIT-------------
+
+
+async function readFiles() {
+    const data1 = await fs.readFile('data-1.json', 'utf-8');
+    console.log("Read data file 1");
+
+    const data2 = await fs.readFile('data-2.json', 'utf-8');
+    console.log("Read data file 2");
+
+    const data3 = await fs.readFile('data-2.json', 'utf-8');
+    console.log("Read data file 3");
+
     console.log(data1, data2, data3);
-}).catch((error) => {
-    console.log(error);
-});
+
+    console.log("All files readed ...");
+
+}
+
+readFiles();
