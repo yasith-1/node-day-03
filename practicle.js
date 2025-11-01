530fe76f159c6e1be876369f7b0dfa7e3eb811fd
Yashith_Prabhashwara <141606159+yasith-1@users.noreply.github.com>
2025-11-01 17:44:09 +0530

    ..

--- practicle.js
+++ practicle.js
@@ -14,12 +14,12 @@ const fs = require('fs').promises;
 
 // Call back hell--------------------------------------------------------------
 // console.log('Starting to reading files...');
-// fs.readFile('data-1.json', 'utf-8', (err, data1) => {
+// fs.readFile('./data/data-1.json', 'utf-8', (err, data1) => {
 //     if (err) return console.error('file 1 error', err);
-//     fs.readFile('data-2.json', 'utf-8', (err, data2) => {
+//     fs.readFile('./data/data-2.json', 'utf-8', (err, data2) => {
 //         if (err) return console.error('file 2 error', err);
 
-//         fs.readFile('data-3.json', 'utf-8', (err, data3) => {
+//         fs.readFile('./data/data-3.json', 'utf-8', (err, data3) => {
 //             if (err) return console.error('file 3 error', err);
 //             console.log(data1, data2, data3);
 //         })
@@ -33,12 +33,12 @@ const fs = require('fs').promises;
 
 // Use promises to avoid the callback hell----------------------------------------
 
-// fs.readFile('data-1.json', 'utf-8').then((data1) => {
+// fs.readFile('./data/data-1.json', 'utf-8').then((data1) => {
 //     console.log("Read file one");
-//     return fs.readFile('data-2.json', 'utf-8').then((data2) => ({ data1, data2 }));
+//     return fs.readFile('./data/data-2.json', 'utf-8').then((data2) => ({ data1, data2 }));
 // }).then(({ data1, data2 }) => {
 //     console.log("Read file two");
-//     return fs.readFile('data-3.json', 'utf-8').then((data3) => ({ data1, data2, data3 }));
+//     return fs.readFile('./data/data-3.json', 'utf-8').then((data3) => ({ data1, data2, data3 }));
 // }).then(({ data1, data2, data3 }) => {
 //     console.log("All Files Read!");
 //     console.log(data1, data2, data3);
@@ -51,13 +51,13 @@ const fs = require('fs').promises;
 
 
 async function readFiles() {
-    const data1 = await fs.readFile('data-1.json', 'utf-8');
+    const data1 = await fs.readFile('./data/data-1.json', 'utf-8');
     console.log("Read data file 1");
 
-    const data2 = await fs.readFile('data-2.json', 'utf-8');
+    const data2 = await fs.readFile('./data/data-2.json', 'utf-8');
     console.log("Read data file 2");
 
-    const data3 = await fs.readFile('data-2.json', 'utf-8');
+    const data3 = await fs.readFile('./data/data-2.json', 'utf-8');
     console.log("Read data file 3");
 
     console.log(data1, data2, data3);
